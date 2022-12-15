383d921eb4bd8ef8dc24f9f7608aa0ca635b1652
handev-001 <83611297+handev-001@users.noreply.github.com>
2022-12-15 10:32:17 -0600

    lighthouse changes

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-113-1671121903.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[false,true,false,false,false,false],"scrollToTop":false}, '.uagb-block-418604d2' );
+} );
+ })
