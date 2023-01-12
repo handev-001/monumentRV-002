7039a4c1903f9047a276ac2c4ccb84765263d492
handev-001 <11905983+knighthawk0811@users.noreply.github.com>
2023-01-12 13:33:22 -0600

    upload media

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-113-1670964988.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false}, '.uagb-block-d3e9803d' );
+} );
+ })
