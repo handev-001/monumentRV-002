48416fc868f83d6eae9f3faa0ed0f127fbe4500b
handev-001 <11905983+knighthawk0811@users.noreply.github.com>
2023-02-09 12:35:45 -0600

    update pricing

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-113-1675967550.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[false,true,false,false,false,false],"scrollToTop":false}, '.uagb-block-3f5a0f67' );
+} );
+ })
