3fda49f45294a4d7db0510ff8d802989acfdf9bf
handev-001 <11905983+knighthawk0811@users.noreply.github.com>
2023-08-09 11:25:34 -0500

    policy pages - update wp
    wp 6.3

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-113-1691598241.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[false,true,false,false,false,false],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-3f5a0f67' );
+} );
+ });
